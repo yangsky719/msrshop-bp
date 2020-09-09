7e7fd1a1bc5ab94fddf2b86a631ec6dbd35e7aef
yang <yangt@tangsk.com>
2020-09-09 16:10:40 +0900

    搭建项目架构

---
+++ msrshop-memeber/src/main/java/com/msr/msrshop/member/MsrshopMemeberApplication.java
@@ -0,0 +1,13 @@
+package com.msr.msrshop.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsrshopMemeberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsrshopMemeberApplication.class, args);
+	}
+
+}
